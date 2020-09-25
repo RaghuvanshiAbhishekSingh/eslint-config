7c294e48878355ff4b8e60198a4f73e39cbaf6e0
Abhishek Singh <abhishek.s@fnp.com>
2020-09-26 02:04:50 +0530

    checking thst its creating issue or not

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, {     Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-
-
-
-
-
-class App extends     Component {
-
+class App extends Component {
   componentDidMount() {
-    if(typeof x !== undefined){
-      console.log('pppp')
+    if (typeof x !== "undefined") {
+      // console.log("pppp");
     }
   }
 
   render() {
     return (
-      <div className="App">
+      <div className="App">  
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
       </div>
     );
